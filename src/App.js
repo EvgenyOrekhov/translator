4437ce9c2a6a836de3bf411d5b0811dd3b4452c6
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2020-04-12 15:38:04 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,57 +1,133 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDebouncedCallback } from "use-debounce";
+import { formatRelative } from "date-fns";
 
-function App() {
+const instance = axios.create({
+  baseURL: "https://translate.yandex.net/api/v1.5/tr.json/",
+});
+
+function mergeParams(params) {
+  return {
+    key:
+      "trnsl.1.1.20200412T100451Z.4ac199cf689eab8d.26e9890d9ff3681a1382cbbdb0ddf3c29ef4476d",
+    ...params,
+  };
+}
+
+function App({ state, actions }) {
+  useEffect(() => {
+    (async function () {
+      const { data } = await instance.get("/getLangs", {
+        params: mergeParams({ ui: "ru" }),
+      });
+      actions.languages.set(data.langs);
+    })();
+  }, [actions.languages]);
+
+  const [translate] = useDebouncedCallback(async function translate() {
+    console.log(state);
+    const { data } = await instance.get("/translate", {
+      params: mergeParams({
+        ui: "ru",
+        text: state.text,
+        lang: `${state.from}-${state.to}`,
+      }),
+    });
+    actions.translation.set(data.text[0]);
+  }, 1000);
+
+  useEffect(() => {
+    if (state.text.trim() !== "") {
+      translate();
+    }
+  }, [state.text, state.from, state.to, translate]);
+
+  function LanguageSelector({ type }) {
+    return (
+      <select
+        className="language"
+        onChange={(event) => actions[type].set(event.target.value)}
+        value={state[type]}
+      >
+        {Object.entries(state.languages)
+          .sort((a, b) => a[1].localeCompare(b[1]))
+          .map(([key, language]) => (
+            <option key={key} value={key}>
+              {language}
+            </option>
+          ))}
+      </select>
+    );
+  }
+
+  function handleChange(event) {
+    actions.setText(event.target.value);
+  }
+
+  function handleSave() {
+    actions.save(Date.now());
+  }
+
   return (
     <div>
-      <div class="languages">
-        <select class="language">
-          <option>Русский</option>
-          <option>English</option>
-        </select>
-        <button type="button" class="switch">
+      <div className="languages">
+        <LanguageSelector type="from" />
+        <button type="button" className="switch" onClick={actions.switch}>
           ⇄
         </button>
-        <select class="language">
-          <option>Русский</option>
-          <option>English</option>
-        </select>
+        <LanguageSelector type="to" />
       </div>
-      <div class="from">
-        <textarea placeholder="Введите текст для перевода"></textarea>
-        <button type="button" class="clear">
-          ✕
-        </button>
+      <div className="from">
+        <textarea
+          placeholder="Введите текст для перевода"
+          value={state.text}
+          onChange={handleChange}
+        ></textarea>
+        {state.text && (
+          <button type="button" className="clear" onClick={actions.clear}>
+            ✕
+          </button>
+        )}
       </div>
-      <small class="characters-left">9999</small>
-      <div class="from">
-        <textarea readOnly></textarea>
-        <button type="button" class="save">
-          ➔
-        </button>
+      <small className="characters-left">{9999 - state.text.length}</small>
+      <div className="from">
+        <textarea readOnly value={state.translation}></textarea>
+        {state.translation && (
+          <button type="button" className="save" onClick={handleSave}>
+            ➔
+          </button>
+        )}
       </div>
       <footer>
-        <div class="history-header">
-          <div class="history-title">История</div>
-          <button type="button">См все</button>
+        <div className="history-header">
+          <div className="history-title">История</div>
+          {/* <button type="button">См все</button> */}
         </div>
-        <div class="empty">
-          <h2>Пусто</h2> Нет последних переводов
-        </div>
-        <div>
-          <div class="history-item-from">
-            <small class="history-item-header">
-              <div>Русский</div>
-              <div>Сегодня в 01:02</div>
-            </small>
-            <div>Привет</div>
+        {state.history.length === 0 ? (
+          <div className="empty">
+            <h2>Пусто</h2>
+            Нет последних переводов
           </div>
-          <div class="history-item-to">
-            <small class="history-item-header">
-              <div>Английский</div>
-            </small>
-            <div>Hello</div>
-          </div>
-        </div>
+        ) : (
+          state.history.map(({ from, to, text, translation, timestamp }) => (
+            <div key={timestamp} class="history-item">
+              <div className="history-item-from">
+                <small className="history-item-header">
+                  <div>{state.languages[from]}</div>
+                  <div>{formatRelative(timestamp, Date.now())}</div>
+                </small>
+                <div>{text}</div>
+              </div>
+              <div className="history-item-to">
+                <small className="history-item-header">
+                  <div>{state.languages[to]}</div>
+                </small>
+                <div>{translation}</div>
+              </div>
+            </div>
+          ))
+        )}
       </footer>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { init } from "actus";
+import logger from "actus-logger";
+import defaultActions from "actus-default-actions";
+import localStoragePlugin from "actus-localstorage";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const initialState = {
+  languages: [],
+  from: "en",
+  to: "ru",
+  text: "",
+  translation: "",
+  history: [],
+};
+
+init([
+  logger(),
+  defaultActions(initialState),
+  {
+    state: initialState,
+    actions: {
+      setText: (text, state) => ({
+        ...state,
+        text,
+        translation: text === "" ? "" : state.translation,
+      }),
+      switch: (ignore, state) => ({
+        ...state,
+        from: state.to,
+        to: state.from,
+        text: state.translation,
+      }),
+      clear: (ignore, state) => ({ ...state, text: "", translation: "" }),
+      save: (timestamp, state) =>
+        state.text === ""
+          ? state
+          : {
+              ...state,
+              history: [
+                {
+                  from: state.from,
+                  to: state.to,
+                  text: state.text,
+                  translation: state.translation,
+                  timestamp,
+                },
+                ...state.history,
+              ],
+            },
+    },
+    subscribers: [
+      ({ state, actions }) => {
+        ReactDOM.render(
+          <React.StrictMode>
+            <App state={state} actions={actions} />
+          </React.StrictMode>,
+          document.getElementById("root")
+        );
+      },
+    ],
+  },
+  localStoragePlugin({
+    selector: (state) => ({
+      from: state.from,
+      to: state.to,
+      history: state.history,
+    }),
+  }),
+]);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
